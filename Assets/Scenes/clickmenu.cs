c6a0e9421cfde825eb508e557f95a32672a5258f
kazuzukan <banghaji363@gmail.com>
2020-04-27 10:40:57 +0700

    add menu scene

---
+++ Assets/Scenes/clickmenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    Application.LoadLevel(0);
+    }
+
+    public void next(){
+        Application.LoadLevel(1);
+    }
+
+    public void quit(){
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
